a9703a9ece0d9b36d4ed8e6d09ba0bbed03ac14e
ashroflukman-spec <ashroflukman@gmail.com>
2026-03-01 01:33:49 +0800

    Fix podcasts API

--- api/podcasts.js
+++ api/podcasts.js
@@ -1,5 +1,4 @@
-import podcasts from '../data/podcasts.json';
-
 export default function handler(req, res) {
+  const podcasts = require('../data/podcasts.json');
   res.status(200).json(podcasts);
 }
