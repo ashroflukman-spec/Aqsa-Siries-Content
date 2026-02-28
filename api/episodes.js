8b3af0aa38318443cb3779a5636561c9a68b402e
ashroflukman-spec <ashroflukman@gmail.com>
2026-03-01 01:35:01 +0800

    Fix episodes API

--- api/episodes.js
+++ api/episodes.js
@@ -1,5 +1,4 @@
-import episodes from '../data/episodes.json';
-
 export default function handler(req, res) {
+  const episodes = require('../data/episodes.json');
   res.status(200).json(episodes);
 }
